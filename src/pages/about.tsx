5201f4f1e7b43a70012dfa0174bcb3525ebe7d0a
Will Harris <will.t.harris@protonmail.com>
2020-02-10 10:41:42 -0800

    feat: add simple about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => (
+  <main>
+    <h1>About Me</h1>
+    <p>Welcome to my Gatsby site</p>
+    <Link to="/">Go back to the homepage</Link>
+  </main>
+)
+
+export default AboutPage
