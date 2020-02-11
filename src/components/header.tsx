f9bf2a1e5f584d6c419ffe5bc0f36509762e2c02
Will Harris <will.t.harris@protonmail.com>
2020-02-11 15:34:35 -0800

    refactor: make header tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <div className="flex bg-purple-400">
+      <h1 className="text-4xl ml-auto mr-6 my-4 font-bold">Bikes & Bytes</h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
