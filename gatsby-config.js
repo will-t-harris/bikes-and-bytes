98a2b00ef4e31d77d5862619a96cfda513866d2e
Will Harris <will.t.harris@protonmail.com>
2020-02-11 15:55:53 -0800

    refactor(config): remove unused options

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,14 +18,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
