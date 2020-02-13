d1ba9ce45cd481e1f505f194425317408f5a6692
Will Harris <will.t.harris@protonmail.com>
2020-02-13 14:35:49 -0800

    style(button): add hover

--- src/components/SideBarButton.tsx
+++ src/components/SideBarButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const SideBarButton = ({ buttonTitle, buttonSlug }) => (
   <>
-    <button className="content-end py-6 bg-gray-200">
+    <button className="content-end py-6 bg-gray-200 hover:bg-gray-400">
       <Link to={buttonSlug}> {buttonTitle}</Link>
     </button>
   </>
