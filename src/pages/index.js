6e456f6ce05a1be37b09de89a9508d01c028e31d
Will Harris <will.t.harris@protonmail.com>
2020-02-13 14:32:42 -0800

    chore(pages): fix imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+
     <Link className="mr-6 font-bold" to="/page-2/">
       Go to page 2
     </Link>
