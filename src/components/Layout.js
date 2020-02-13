2a32fca81581a0dfd5106b01ab414f8c07765443
Will Harris <will.t.harris@protonmail.com>
2020-02-13 14:12:11 -0800

    chore: fix capitalization

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import SideBar from "./sidebar"
+import Header from "./Header"
+import SideBar from "./SideBar"
 import "../../src/index.css"
 
 const Layout = ({ children }) => {
