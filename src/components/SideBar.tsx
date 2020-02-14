d86ddaa9c44812c27e174d5641bafb0b96ff46cc
Will Harris <will.t.harris@protonmail.com>
2020-02-13 16:04:26 -0800

    style(sidebar): change sidebar/button styles

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import SideBarButton from "./SideBarButton"
 
 const SideBar = () => (
-  <aside className="float-left w-1/5 h-full flex flex-col">
+  <aside className="bg-gray-400 float-left w-1/5 h-full flex flex-col">
     <SideBarButton buttonTitle="Bytes" buttonSlug="bytes" />
     <SideBarButton buttonTitle="Bikes" buttonSlug="bikes" />
     <SideBarButton buttonTitle="Routes" buttonSlug="routes" />
--- src/components/SideBarButton.tsx
+++ src/components/SideBarButton.tsx
@@ -3,9 +3,12 @@ import { Link } from "gatsby"
 
 const SideBarButton = ({ buttonTitle, buttonSlug }) => (
   <>
-    <button className="content-end py-6 bg-gray-200 hover:bg-gray-400">
-      <Link to={buttonSlug}> {buttonTitle}</Link>
-    </button>
+    {" "}
+    <Link to={buttonSlug} className="bg-gray-200 hover:bg-gray-400">
+      <button className="flex justify-center w-1/2 py-6 my-4 mx-auto bg-gray-400 rounded">
+        {buttonTitle}
+      </button>
+    </Link>
   </>
 )
 
