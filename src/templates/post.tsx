bf76731a19d23aa8510f880d685ca0c04000c643
Will Harris <will.t.harris@protonmail.com>
2020-02-20 08:29:46 -0800

    feat: add links to post template

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 const Post = ({ data }) => {
   const { markdownRemark: post } = data
   return (
     <div>
       <h1>{post.frontmatter.title}</h1>
+      <Link to="/bytes" style={{ display: "block" }}>
+        Back to blog posts
+      </Link>
+      <Link to="/">Return to home page</Link>
     </div>
   )
 }
