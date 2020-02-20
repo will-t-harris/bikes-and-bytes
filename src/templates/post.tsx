f70edc84f834c632fe124bd5d01163034f56888d
Will Harris <will.t.harris@protonmail.com>
2020-02-20 15:16:39 -0800

    feat: add layout component to post template

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,18 +1,21 @@
 import React from "react"
 import Helmet from "react-helmet"
+import Layout from "../components/Layout"
 import { graphql, Link } from "gatsby"
 
 const Post = ({ data }) => {
   const { markdownRemark: post } = data
   return (
-    <div>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <Link to="/bytes" style={{ display: "block" }}>
-        Back to blog posts
-      </Link>
-      <Link to="/">Return to home page</Link>
-    </div>
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Link to="/bytes" style={{ display: "block" }}>
+          Back to blog posts
+        </Link>
+        <Link to="/">Return to home page</Link>
+      </div>
+    </Layout>
   )
 }
 
