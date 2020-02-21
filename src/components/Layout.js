16bd895aa08f56e4a37f465f86e2e16704b70c1c
Will Harris <will.t.harris@protonmail.com>
2020-02-21 15:51:35 -0800

    refactor: remove sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
-import SideBar from "./SideBar"
 import "../../src/index.css"
 
 const Layout = ({ children }) => {
@@ -27,7 +26,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <SideBar />
       <div>
         <main>{children}</main>
       </div>
