46f9b34bf4087205a6883ceb6179deb453fe2834
Will Harris <will.t.harris@protonmail.com>
2020-02-21 16:02:20 -0800

    config purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // specify paths to all template files in project
+  content: ["./src/**/*.html", "./src/**/*.tsx", "./src/**/*.js"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+})
+
 module.exports = () => ({
-  plugins: [require("tailwindcss", require("autoprefixer"))],
+  plugins: [
+    require("tailwindcss", require("autoprefixer"), ...(process.env.NODE_ENV ===
+    "production"
+      ? [purgecss]
+      : [])),
+  ],
 })
