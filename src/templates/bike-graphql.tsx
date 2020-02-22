1f563d5d49ca3424fa688f905522d91c89a83caa
Will Harris <will.t.harris@protonmail.com>
2020-02-21 16:44:27 -0800

    feat: make footer section sticky

--- src/templates/bike-graphql.tsx
+++ src/templates/bike-graphql.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
+import Layout from "../components/Layout"
 
 // define query for a single bike
 export const query = graphql`
@@ -25,21 +26,21 @@ const Bike = ({ data }) => {
 
   return (
     <>
-      <div className="flex">
+      <Layout>
         <h1 className="text-center">{bike.title}</h1>
         <Image
           fluid={bike.image.childImageSharp.fluid}
           alt={bike.title}
-          style={{ marginRight: "1rem", width: 500 }}
+          className="absolute"
         />
-        <p>{bike.description}</p>
-      </div>
-      <Link to="/" style={{ display: "block" }}>
-        Return to homepage
-      </Link>
-      <Link to="/bike" style={{ display: "block" }}>
-        Return to bikes
-      </Link>
+        <p className="text-center">{bike.description}</p>
+        <Link to="/" style={{ display: "block" }}>
+          Return to homepage
+        </Link>
+        <Link to="/bike" style={{ display: "block" }}>
+          Return to bikes
+        </Link>
+      </Layout>
     </>
   )
 }
