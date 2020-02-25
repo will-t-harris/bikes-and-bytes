e756813d30a17109c27aa2a4426ab5164f24df77
Will Harris <will.t.harris@protonmail.com>
2020-02-25 01:02:04 -0800

    style(contact): very basic form styling

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,27 +1,46 @@
-import React, { useState, FC } from "react"
+import React, { FC } from "react"
 
 const ContactForm: FC = () => {
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" required />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" required />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" required></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <div className="border border-blueGray-700">
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name:{" "}
+            <input className="my-4 rounded" type="text" name="name" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email:{" "}
+            <input
+              className="my-4 rounded"
+              type="email"
+              name="email"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:{" "}
+            <textarea
+              className="rounded w-1/6"
+              name="message"
+              required
+            ></textarea>
+          </label>
+        </p>
+        <p>
+          <button
+            className="my-4 border-blueGray-900 border-2 px-4 py-2 rounded hover:bg-blueGray-900 hover:text-gray-200"
+            type="submit"
+          >
+            Send
+          </button>
+        </p>
+      </form>
+    </div>
   )
 }
 
