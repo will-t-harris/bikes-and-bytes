805bfb0ba29c268ea9881b6486b4bb89f68bea76
Will Harris <will.t.harris@protonmail.com>
2020-02-24 16:50:08 -0800

    feat: add SEO component to templates

--- src/templates/bike-graphql.tsx
+++ src/templates/bike-graphql.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
 import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 // define query for a single bike
 export const query = graphql`
@@ -27,6 +28,7 @@ const Bike = ({ data }) => {
   return (
     <>
       <Layout>
+        <SEO title={bike.title} />
         <h1 className="text-center">{bike.title}</h1>
         <Image
           fluid={bike.image.childImageSharp.fluid}
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -2,11 +2,13 @@ import React from "react"
 import Helmet from "react-helmet"
 import Layout from "../components/Layout"
 import { graphql, Link } from "gatsby"
+import SEO from "../components/SEO"
 
 const Post = ({ data }) => {
   const { markdownRemark: post } = data
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
