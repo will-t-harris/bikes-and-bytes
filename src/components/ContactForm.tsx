f8926425ae853be03cfdf8d9d08ecf69d631995e
Will Harris <will.t.harris@protonmail.com>
2020-02-25 00:39:24 -0800

    refactor: setup netlify form syntax

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,69 +2,26 @@ import React, { useState, FC } from "react"
 import axios from "axios"
 
 const ContactForm: FC = () => {
-  const [state, setState] = useState({
-    name: "",
-    message: "",
-    email: "",
-    sent: false,
-    buttonText: "Send Message",
-  })
-
-  const handleFormSubmit = event => {
-    event.preventDefault()
-
-    setState({ ...state, buttonText: "...sending" })
-
-    let data = {
-      name: state.name,
-      email: state.email,
-      message: state.message,
-    }
-
-    axios
-      .post("API_URI", data)
-      .then(res => {
-        setState({ ...state, sent: true })
-        resetForm()
-      })
-      .catch(() => {
-        console.log("Message not sent...") // TODO HANDLE ERROR CASE
-      })
-  }
-
-  const resetForm = () => {
-    setState({
-      name: "",
-      message: "",
-      email: "",
-      sent: true,
-      buttonText: "Message Sent!",
-    })
-  }
-
   return (
-    <form onSubmit={handleFormSubmit}>
-      <label htmlFor="message-input">Your Message</label>
-      <textarea
-        name="message"
-        placeholder="Please write your message here"
-        value="state.message"
-        required
-      />
-
-      <label htmlFor="message-name">Your Name</label>
-      <input
-        onChange={setState({ ...state, email: "" })}
-        name="email"
-        type="email"
-        placeholder="Your email address?"
-        required
-        value={state.email}
-      />
-
-      <div className="">
-        <button type="submit">{state.buttonText}</button>
-      </div>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" required />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" required />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" required></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   )
 }
