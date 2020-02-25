70a0bc287c67ce9a891e186e2c1146ced34b8b91
Will Harris <will.t.harris@protonmail.com>
2020-02-24 23:04:13 -0800

    style: add colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blueGray-900": "#102a43",
+        "blueGray-800": "#243b53",
+        "blueGray-700": "#334e68",
+        "blueGray-600": "#486581",
+        "blueGray-500": "#627d98",
+        "blueGray-400": "#829ab1",
+        "blueGray-300": "#9fb3c8",
+        "blueGray-200": "#bcccdc",
+        "blueGray-100": "#d9e2ec",
+      },
+    },
   },
   variants: {},
   plugins: [],
