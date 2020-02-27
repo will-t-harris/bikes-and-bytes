d4ac9cf5566bceaa633c6baf1dd214e172c3d2a2
Will Harris <will.t.harris@protonmail.com>
2020-02-26 16:53:10 -0800

    feat: add short about me blurb (wip)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>About Me</h1>
-    <p>Welcome to the about page</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1 className="text-xl">About Me</h1>
+    <p>I'm a web developer currently living in San Francisco. </p>
+    <p>
+      I'm interested in open source software, online privacy, and web security.
+    </p>
   </Layout>
 )
 
