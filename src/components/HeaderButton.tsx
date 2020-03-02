f35860c34a9dfada0a89e99c63d99da5af5c6101
Will Harris <will.t.harris@protonmail.com>
2020-03-02 14:31:19 -0800

    style(header): add shadow

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const HeaderButton = ({ buttonTitle, buttonSlug, buttonClass }) => (
   <>
-    <div className="flex-auto">
+    <div className="flex-auto shadow-2xl">
       <Link to={buttonSlug} className={buttonClass}>
         {buttonTitle}
       </Link>
