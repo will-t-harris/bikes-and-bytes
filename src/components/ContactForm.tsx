f154ee3055d3360199d44e22792b160c9d507777
Will Harris <will.t.harris@protonmail.com>
2020-03-05 11:51:29 -0800

    feat: control contact input fields

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,29 +1,47 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 
 const ContactForm: FC = () => {
+  const [state, setState] = useState({ name: "", email: "", message: "" })
+
+  const handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+
+    setState({ [name]: value })
+  }
+
   return (
-    <div className="border border-blueGray-700">
+    <div className="flex justify-center border border-blueGray-700">
       <form
         name="contact"
         method="POST"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        className="text-col"
       >
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name:{" "}
-            <input className="my-4 rounded" type="text" name="name" required />
+            <input
+              className="my-4 rounded contact-input"
+              type="text"
+              name="name"
+              value={state.name}
+              onChange={handleInputChange}
+              required
+            />
           </label>
         </p>
         <p>
           <label>
             Your Email:{" "}
             <input
-              className="my-4 rounded"
+              className="my-4 rounded contact-input"
               type="email"
               name="email"
+              value={state.email}
+              onChange={handleInputChange}
               required
             />
           </label>
@@ -32,19 +50,24 @@ const ContactForm: FC = () => {
           <label>
             Message:{" "}
             <textarea
-              className="rounded w-1/6"
+              className="rounded contact-input"
               name="message"
+              cols={22}
+              value={state.message}
+              onChange={handleInputChange}
               required
             ></textarea>
           </label>
         </p>
         <p>
-          <button
-            className="my-4 border-blueGray-900 border-2 px-4 py-2 rounded hover:bg-blueGray-900 hover:text-gray-200"
-            type="submit"
-          >
-            Send
-          </button>
+          <div className="flex justify-end">
+            <button
+              className="my-4 border-blueGray-900 border-2 px-4 py-2 rounded hover:bg-blueGray-900 hover:text-gray-200"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
         </p>
       </form>
     </div>
