85d5f3436f892fb55df7436df41ed1f4572af688
Will Harris <will.t.harris@protonmail.com>
2020-03-05 11:51:19 -0800

    content: add contact subtitle

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,8 +8,12 @@ const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
     <h1 className="py-16 font-serif text-4xl text-gray-900 font-bold text-col">
-      Contact
+      Get in touch!
     </h1>
+    <p className="py-4 font-serif text-xl text-gray-900 font-bold text-col">
+      Want to work together? Got an awesome project in mind? Drop me a line
+      through the contact form below!
+    </p>
     <ContactForm />
   </Layout>
 )
