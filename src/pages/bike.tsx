09d4d85db88b07fadd29bb26bd8d2f8ae41b8733
Will Harris <will.t.harris@protonmail.com>
2020-03-05 09:49:04 -0800

    style: tweak bike page style

--- src/pages/bike.tsx
+++ src/pages/bike.tsx
@@ -9,15 +9,21 @@ const BikesPage = () => (
     <h1 className="text-col">Bikes</h1>
     <p className="text-col">Welcome to the bikes page</p>
     <div className="text-col">
-      <Link to="/bike/lht" className="px-4 text-green-800 hover:text-green-400">
-        LHT
+      <Link
+        to="/bike/lht"
+        className="px-4 ml-10 text-green-800 hover:text-green-500 hover:underline"
+      >
+        Long Haul Trucker
       </Link>
-      <Link to="/bike/krampus" className="px-4 text-red-800 hover:text-red-400">
+      <Link
+        to="/bike/krampus"
+        className="px-4 text-red-800 hover:text-red-500 hover:underline"
+      >
         Krampus
       </Link>
       <Link
         to="/bike/unit"
-        className="px-4 text-purple-700 hover:text-purple-400"
+        className="px-4 text-purple-700 hover:text-purple-500 hover:underline"
       >
         Unit
       </Link>
