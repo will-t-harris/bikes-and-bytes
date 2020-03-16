2b63ca885b512bb3d9997a5aa27d797f2744ca45
Will Harris <will.t.harris@protonmail.com>
2020-03-15 22:22:07 -0700

    style: create about text class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,17 @@ const IndexPage = () => (
   <>
     <SEO title="Home" />
     <div className="grid grid-cols-4 py-10">
-      <h1 className="col-start-2 font-serif text-4xl text-gray-900 font-bold">
+      <h1 className="col-start-2 font-mono text-4xl text-gray-900 font-bold">
         <Typewriter />
       </h1>
-      <p className="mt-10 col-start-2 col-span-2">
+      <p className="about-text">
         I'm a developer currently living in San Francisco.{" "}
       </p>
-      <p className="mt-10 col-start-2 col-span-2">
+      <p className="about-text">
         I'm interested in open source software, online privacy, cryptography,
         and web security.
       </p>
-      <p className="mt-10 col-start-2 col-span-2">
+      <p className="about-text">
         I'm passionate about bikes--I think they make the world a better place.
         I'm happiest bumping along on a dirt road on a sunny day in the middle
         of nowhere carrying all that I need on my bike.
