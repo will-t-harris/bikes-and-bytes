9d82a7c12e0888f4cb25608abefa53e974444b4c
Will Harris <will.t.harris@protonmail.com>
2020-04-21 10:35:22 -0700

    destructure data

--- src/templates/postTemplate.tsx
+++ src/templates/postTemplate.tsx
@@ -2,20 +2,25 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/SEO"
 
-const Post = ({ data }) => (
-  <>
-    <SEO title={data.markdownRemark.frontmatter.title} />
-    <div className="grid grid-cols-4">
-      <h1 className="pt-16 col-start-2 col-span-2 text-2xl font-semibold">
-        {data.markdownRemark.frontmatter.title}
-      </h1>
-      <div
-        className="my-10 col-start-2 col-span-2"
-        dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-      />
-    </div>
-  </>
-)
+const Post = ({ data }) => {
+  const { markdownRemark } = data
+  const title = markdownRemark.frontmatter.title
+  const html = markdownRemark.html
+  return (
+    <>
+      <SEO title={title} />
+      <div className="grid grid-cols-4">
+        <h1 className="pt-16 col-start-2 col-span-2 text-2xl font-semibold">
+          {title}
+        </h1>
+        <div
+          className="my-10 col-start-2 col-span-2 leading-8"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </>
+  )
+}
 
 export default Post
 
