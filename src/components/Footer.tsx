9e42f1cd722890a0d40a5e6f65bae0c70a34ffc2
Will Harris <will.t.harris@protonmail.com>
2020-04-28 23:03:50 -0700

    shorten transition

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import LinkedinIcon from "./icons/LinkedinIcon"
 
 const Footer = () => {
   const iconVisualTheme =
-    "fill-current text-gray-400 hover:text-pink-600 transition ease-in-out duration-300"
+    "fill-current text-gray-400 hover:text-pink-600 transition ease-in-out duration-150"
 
   return (
     <footer className="w-full flex text-center py-2 px-12 bg-gray-800">
