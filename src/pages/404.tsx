c7d39d1f1444a74b579ffa867b229e07b19026c4
Will Harris <will.t.harris@protonmail.com>
2020-05-12 20:34:09 -0700

    add pathname to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import SEO from "../components/SEO"
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+interface Props {
+  data: any
+}
+
+const NotFoundPage = ({ data }: Props) => {
+  const path = data.sitePage.path
+
+  return (
+    <>
+      <SEO title="404: Not found" pathname={`${path}`} />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </>
+  )
+}
 
 export default NotFoundPage
+
+export const query = graphql`
+  query fourOhFour {
+    sitePage(component: { regex: "/404/" }, id: { eq: "SitePage /404/" }) {
+      path
+    }
+  }
+`
