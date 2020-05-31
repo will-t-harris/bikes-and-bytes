510d55316e78aadb7857ad32f44abe12f0026d24
Will Harris <will.t.harris@protonmail.com>
2020-05-30 17:32:08 -0700

    refactor: use header and nav tags

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,14 @@ import CodeIcon from "./icons/CodeIcon"
 
 const Header = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-navBackground px-6 site-linear-gradient">
-      <div className="flex mx-auto py-4 items-center flex-shrink-0 lg:mr-6 lg:ml-0">
+    <header className="flex items-center justify-between flex-wrap bg-navBackground px-6 site-linear-gradient">
+      <nav className="flex mx-auto py-4 items-center flex-shrink-0 lg:mr-6 lg:ml-0">
         <Link to="/" className="lg:ml-6 lg:mr-6 flex self-center flex-row">
-          <BikeIcon />
-          <CodeIcon />
+          <BikeIcon iconTitle="bikeIcon" />
+          <CodeIcon iconTitle="codeIcon" />
         </Link>
-      </div>
-      <div className="w-full flex lg:w-auto">
+      </nav>
+      <nav className="w-full flex lg:w-auto">
         <HeaderButton
           buttonTitle="Bytes"
           buttonSlug="/bytes"
@@ -25,8 +25,8 @@ const Header = () => {
           buttonSlug="/bikes"
           buttonClass="header-btn inline-block lg:flex lg:mt-0 lg:mr-12"
         />
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
