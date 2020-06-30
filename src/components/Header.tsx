2eb1988ab470e5218e3fef00cc535c4bb14c8882
Will Harris <will.t.harris@protonmail.com>
2020-06-29 17:11:12 -0700

    feat: start hacking together dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,48 @@
-import React from "react"
+import React, { Dispatch, SetStateAction } from "react"
 import { Link } from "gatsby"
 
 import { HeaderButton } from "./"
 import { BikeIcon, CodeIcon } from "./icons"
 
-const Header = () => {
+interface Props {
+  setDarkMode: Dispatch<SetStateAction<boolean>>
+  darkMode: boolean
+}
+
+const Header = ({ darkMode, setDarkMode }: Props) => {
+  const darkModeClasses = darkMode ? "dark-mode" : "light-mode"
+
   return (
-    <header className="flex items-center justify-between flex-wrap bg-navBackground px-6 site-linear-gradient">
+    <header
+      className={`${darkModeClasses} flex items-center justify-between flex-wrap px-6 site-linear-gradient`}
+    >
       <nav className="flex mx-auto py-4 items-center flex-shrink-0 lg:mr-6 lg:ml-0">
         <Link to="/" className="lg:ml-6 lg:mr-6 flex self-center flex-row">
           <BikeIcon iconTitle="bikeIcon" />
           <CodeIcon iconTitle="codeIcon" />
         </Link>
+      </nav>
+      <nav>
+        <div>
+          <span
+            className={`${
+              darkMode ? "text-pink-400" : "text-green-400"
+            } text-2xl`}
+          >
+            ☀️
+          </span>
+          <span>
+            <input
+              checked={darkMode}
+              onChange={() => setDarkMode(prevMode => !prevMode)}
+              type="checkbox"
+              name="toggleCheckbox"
+              id="toggleCheckbox"
+            />
+            <label htmlFor="toggleCheckbox"></label>
+          </span>
+          <span className="text-yellow-400 text-xl">🌙</span>
+        </div>
       </nav>
       <nav className="w-full flex lg:w-auto">
         <HeaderButton
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Header, Footer } from "./index"
 
@@ -7,10 +7,38 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+  const [darkMode, setDarkMode] = useState(getInitialMode())
+
+  useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode))
+  }, [darkMode])
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage
+    const savedMode = JSON.parse(localStorage.getItem("dark") || "true")
+    const userPrefersDark = getPrefColorScheme()
+    console.log(userPrefersDark)
+    if (userPrefersDark) {
+      return true
+    } else if (isReturningUser) {
+      return savedMode
+    } else {
+      return false
+    }
+  }
+
+  function getPrefColorScheme() {
+    if (!window.matchMedia) return
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+  }
+
+  const darkModeClasses = darkMode ? "dark-mode" : "light-mode"
+
   return (
     <>
-      <Header />
-      <main className="flex-1 bg-bodyBackground text-white min-h-screen">
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <main className={`${darkModeClasses} flex-1 min-h-screen`}>
         {children}
       </main>
       <Footer />
